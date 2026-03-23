6ef3bedd7743b8685095738797a35ab01287a830
M1nutka <mihailuraskin@gmail.com>
2026-03-23 15:05:45 +0400

    add /record path

--- src/main/java/lite/vls/VlsController.java
+++ src/main/java/lite/vls/VlsController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 
@@ -20,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-
+@RequestMapping("/record")
 public class VlsController {
 
     private final VlsService vlsService;
