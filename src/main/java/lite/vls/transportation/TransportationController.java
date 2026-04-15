d753b94f4c23394ef10a445174ac19e62d4d4455
M1nutka <mihailuraskin@gmail.com>
2026-04-15 14:16:58 +0400

    update controller

--- src/main/java/lite/vls/transportation/TransportationController.java
+++ src/main/java/lite/vls/transportation/TransportationController.java
@@ -3,8 +3,6 @@ package lite.vls.transportation;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.NoSuchElementException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -27,33 +25,29 @@ import org.springframework.web.bind.annotation.PutMapping;
 @RequestMapping("/record")
 public class TransportationController {
 
-    private final TransportationService vlsService;
+    private final TransportationService service;
+
 
     private static final Logger log = LoggerFactory.getLogger(TransportationController.class);
 
-    public TransportationController(TransportationService vlsService){
-        this.vlsService = vlsService;
+    public TransportationController(TransportationService service){
+        this.service = service;
     }
     
     @GetMapping()
     public ResponseEntity<List<TransportationRecord>> getAllRecords() {
         log.info("Get all records");
-        return ResponseEntity.ok(vlsService.getAllRecord());
+        return ResponseEntity.ok(service.getAllRecord());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<TransportationRecord> getById(
         @PathVariable("id") Long id
     ) {
-        try {
-            log.info("Get by id = " + id);
-            return ResponseEntity.ok()
-                .body(vlsService.getById(id));
-        } catch (NoSuchElementException e) {
-            log.error("No such by id = " + id);
-            return ResponseEntity.status(404)
-                .build();
-        }
+        log.info("Get by id = " + id);
+        return ResponseEntity.ok()
+            .body(service.getById(id));
+
     }
     
 
@@ -63,7 +57,7 @@ public class TransportationController {
     ) {
         log.info("Created new record");
         return ResponseEntity.status(HttpStatus.CREATED)
-            .body(vlsService.createRecord(vlsToCreate));
+            .body(service.createRecord(vlsToCreate));
     }
 
     @PutMapping("/{id}")
@@ -71,16 +65,11 @@ public class TransportationController {
         @PathVariable("id") Long id,
         @RequestBody TransportationRecord vlsToUpdate
     ) {
-        try {
-            vlsService.updateRecord(id, vlsToUpdate);
-            log.info("update record by id = " + id);
-            return ResponseEntity.ok()
-                .build();
-        } catch (NoSuchElementException e) {
-            log.error("No such record by id = " + id);
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .build();
-        }
+        service.updateRecord(id, vlsToUpdate);
+        log.info("update record by id = " + id);
+        return ResponseEntity.ok()
+            .build();
+
     }
     
 
@@ -88,31 +77,20 @@ public class TransportationController {
     public ResponseEntity<Void> deleteRecord(
         @PathVariable("id") Long id
     ) {
-        try {
-            vlsService.cancelRecord(id);
-            log.info("Delete record id = " + id);
-            return ResponseEntity.ok()
-                .build();
-        } catch (NoSuchElementException e) {
-            log.error("No such record by id = " + id);
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .build();
-        }
+        service.cancelRecord(id);
+        log.info("Delete record id = " + id);
+        return ResponseEntity.ok()
+            .build();
+
     }
 
     @PostMapping("approve/{id}")
     public ResponseEntity<TransportationRecord> approveRecord(
         @PathVariable("id") Long id
     ) {
-        try {
-            vlsService.approveRecord(id);
-            log.info("Approve record id = " + id);
-            return ResponseEntity.ok()
-                .build();
-        } catch (NoSuchElementException e) {
-            log.error("No such record by id = " + id);
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .build();
-        }
+        service.approveRecord(id);
+        log.info("Approve record id = " + id);
+        return ResponseEntity.ok()
+            .build();
     }
 }
