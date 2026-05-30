feda5d90ccd0fe733503dfe6686951d089a848dd
M1nutka <mihailuraskin@gmail.com>
2026-05-30 13:20:10 +0400

    New test getAllUser

--- src/test/java/lite/vls/VlsApplicationTests.java
+++ src/test/java/lite/vls/VlsApplicationTests.java
@@ -15,8 +15,10 @@ import lite.vls.users.UserRole;
 import lite.vls.users.UserService;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 @ExtendWith(MockitoExtension.class)
 
@@ -69,6 +71,53 @@ class VlsApplicationTests {
 		assertEquals("Иван", result.name());
 		
 		verify(userMapper, times(1)).toDomain(userEntity);
+	}
+
+	@Test
+	void getAllUserTest() {
+
+		Long userId1 = 1L;
+		User user1 = new User(
+			userId1,"Иван","Петров","+7 (999) 123-45-67",
+			"ivan.petrov@example.com","password123",UserRole.USER,true
+		);
+
+		UserEntity userEntity1 = new UserEntity(
+			userId1, "Иван", "Петров","+7 (999) 123-45-67",
+			"ivan.petrov@example.com","password123",UserRole.USER,	true
+		);
+
+		Long userId2 = 2L;
+		User user2 = new User(
+			userId2, "Мария", "Сидорова", "+7 (999) 765-43-21",
+			"maria@example.com", "password456", UserRole.USER, true
+		);
+
+		UserEntity userEntity2 = new UserEntity(
+			userId2, "Мария", "Сидорова", "+7 (999) 765-43-21",
+			"maria@example.com", "password456", UserRole.USER, true
+		);
+
+		List<User> users = Arrays.asList(user1, user2);
+		List<UserEntity> usersEntity = Arrays.asList(userEntity1, userEntity2);
+
+		when(userRepository.findAll()).thenReturn(usersEntity);
+		when(userMapper.toDomain(userEntity1)).thenReturn(user1);
+		when(userMapper.toDomain(userEntity2)).thenReturn(user2);
+		
+
+		List<User> result = userService.getAllUsers();
+
+		assertNotNull(result);
+		assertEquals(2, result.size());
+		assertEquals(users, result);
+		assertEquals(user1, result.get(0));
+		assertEquals("Иван", result.get(0).name());
+		assertEquals(user2, result.get(1));
+		assertEquals("Мария", result.get(1).name());
+		
+		verify(userRepository, times(1)).findAll();
+		verify(userMapper, times(2)).toDomain(any(UserEntity.class));
 	}
 
 }
